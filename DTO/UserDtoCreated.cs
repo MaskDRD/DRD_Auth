2499e6db3e5236f1c117ea52fcb0ef5690099aaf
Maksim <email@mail.ru>
2023-08-31 23:10:00 +0400

    DTO создания пользователя

---
+++ DTO/UserDtoCreated.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace auth.DTO
+{
+    public class UserDtoCreated
+    {
+        [Required]
+        public string? Login { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        [Required]
+        public string? Email { get; set; }
+    }
+}
